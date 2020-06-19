5f4067330fc65ab1f6837badf8823c0168e144dc
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-19 18:52:31 +0100

    Create list_tuple_set.py

---
+++ list_tuple_set.py
@@ -0,0 +1,20 @@
+coding excercise-2
+# Create a list, called my_list, with three numbers. The total of the numbers added together should be 100.
+my_list=[20,20,10]
+
+# Create a tuple, called my_tuple, with a single value in it
+my_tuple=(12,)
+
+# Modify set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5,77,9,12}
+
+set2.intersection(set1)
+
+ # another example for set
+set1={12,15,6,8,19,20}
+set2={15,6,20}
+set3={12,19,20}
+set1=set2.intersection(set3)
+print(set1)
+
