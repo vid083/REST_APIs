a3056480ca9aa6e00df872a0bb8a0da4c1ae7930
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-23 02:41:26 +0100

    Update 03_getting_user_input.py

--- 03_getting_user_input.py
+++ 03_getting_user_input.py
@@ -5,5 +5,5 @@ print(name)
 
 size_input = input("How big is your house (in square feet): ")
 square_feet = int(size_input)
-square_metres = square_feet / 10.8  # Make sure this is correct
+square_metres = square_feet / 10.8  
 print(f"{square_feet} square feet is {square_metres} square metres.")
