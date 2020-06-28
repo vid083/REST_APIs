663aa568f9a5be40fb718bf85e1cbd23f771b756
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-28 05:16:22 +0100

    Create 28_class_composition.py

---
+++ 28_class_composition.py
@@ -0,0 +1,39 @@
+class BookShelf:
+    def __init__(self, quantity):
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"BookShelf with {self.quantity} books."
+
+
+shelf = BookShelf(300)
+
+
+class Book(BookShelf):
+    def __init__(self, name, quantity):
+        super().__init__(quantity)
+        self.name = name
+
+
+book = Book("Harry Potter", 120)
+print(book)  # What?
+
+# -- Composition over inheritance here --
+
+class BookShelf:
+    def __init__(self, *books):
+        self.books = books
+
+    def __str__(self):
+        return f"BookShelf with {len(self.books)} books."
+
+
+class Book:
+    def __init__(self, name):
+        self.name = name
+
+
+book = Book("Harry Potter")
+book2 = Book("Python 101")
+shelf = BookShelf(book, book2)
+print(shelf)
