f5ecec104119959072e6e53786f80f1054b3a3a1
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-28 05:16:45 +0100

    Create 20_dictionary_compr.py

---
+++ 20_dictionary_compr.py
@@ -0,0 +1,25 @@
+users = [
+    (0, "Bob", "password"),
+    (1, "Rolf", "bob123"),
+    (2, "Jose", "longp4assword"),
+    (3, "username", "1234"),
+]
+
+username_mapping = {user[1]: user for user in users}
+userid_mapping = {user[0]: user for user in users}
+
+print(username_mapping)
+
+print(username_mapping["Bob"])  
+
+# -- Can be useful to log in for example --
+
+username_input = input("Enter your username: ")
+password_input = input("Enter your password: ")
+
+_, username, password = username_mapping[username_input]
+
+if password_input == password:
+    print("Your details are correct!")
+else:
+    print("Your details are incorrect.")
