e8324741ae89f301272d1be35fab95b7f8892fd0
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-28 05:16:33 +0100

    Create 24_functn_vs_methods.py

---
+++ 24_functn_vs_methods.py
@@ -0,0 +1,21 @@
+#Functions vs. Methods
+
+#A function that lives inside a class is called a method.
+
+#So, methods are functions, but not all functions are methods.
+
+#Function
+
+def average(sequence):
+    return sum(sequence) / len(sequence)
+
+
+#Method
+
+class Student:
+    def __init__(self):  # method
+        self.name = "Rolf"
+        self.grades = (79, 90, 95, 99)
+    
+    def average(self):  # method
+        return sum(self.grades) / len(self.grades)
