b5a5a896226815a8df1d6c219c8a01eb89457bb8
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-10-01 23:52:07 +0100

    Create in.py

---
+++ in.py
@@ -0,0 +1,5 @@
+friends={"vidya","vivek"}
+print("vidya" in friends)
+visited_places={"london","india","singapore"}
+user_visited= input("Enter something you've recently visited: ")
+print(user_visited in  visited_places)
